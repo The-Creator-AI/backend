d552a156d050f4fc977430127112d5dfb374ee8f
Pulkit Singh <pulkitsingh01@gmail.com>
2024-08-14 23:54:25 +0530

    port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+
+  // Retrieve the port number from the environment variables
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
